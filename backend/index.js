8d8ab3065d954810400fa9a7515f560a139dd6b3
Leopold Arreström <leopold.arrestrom@gmail.com>
2019-09-25 21:46:18 +0200

    clean up

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,8 @@ const db = new TodoDatabase();
 const PORT = 3001
 
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.send('Hello World!'));
 app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS');
     next();
